b75adaed6d853969ee774cd56099bef435cbad70
JinJay <jinjaysnow@gmail.com>
2015-12-17 16:00:09 +0800

    fix sqlite problem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151212022201) do
+ActiveRecord::Schema.define(version: 20151217075550) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
