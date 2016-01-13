d9a075a22207eecdc4fb3c569794bd159de3d174
JinJay <jinjaysnow@gmail.com>
2016-01-13 10:02:00 +0800

    fixed the layout of rails

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     if cookies[:remember_token] == nil
-        print "no user"
         @user = nil
     else
-        print "has user"
         @user = current_user
     end
     render 'index'
