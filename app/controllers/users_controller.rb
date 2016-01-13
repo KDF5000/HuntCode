de6633b631b89ac8ebcab2f7d808bba9cc6393c6
KDF5000 <kdf5000@163.com>
2016-01-13 13:05:19 +0800

    恢复第三方登录的代码

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,7 +70,42 @@ class UsersController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  def github_login
+    require 'json'
+    @code = params[:code]
+    # 获取access_token
+    uri = URI.parse("https://github.com/login/oauth/access_token")
+    data = {client_id:'ea6bbfeb21340b60c869', client_secret:'e6512b8cce7374e6a6d259ee4920b1b8add576f7', code:@code, redirect_uri:"http://localhost:3000/github/login"}
+    res = Net::HTTP.post_form(uri, data)
+    info = res.body.split('&')
+    access_token_arr = info[0].split('=')
+    puts access_token_arr[1]
+    access_token= access_token_arr[1]
+    #获取用户信息
+    uri = URI.parse('https://api.github.com/user?access_token='+access_token)
+    response = Net::HTTP.get(uri)
+    res = JSON.parse response
+    @identifier = res['id']
+    if Thirdparty.exists?(identifier:@identifier)
+      @thirdParty = Thirdparty.find_by_identifier(@identifier)
+      begin
+        @user = User.find(@thirdParty.user_id)
+      rescue
+        puts "查找用户失败"
+        redirect_to loginReg_url
+      end
+    else
+    #创建一个用户
+      puts "创建一个用户"
+      @avatar_url = res['avatar_url']
+      @user_name = res['login']
+      @email = res['email']
+      @user = User.create(:x_username=>@user_name, :avatar=>@avatar_url, :x_email=>@email)
+      puts "user_id:",@user.x_username
+      @thirdParty = Thirdparty.create(:identifier=>@identifier, :user_id=>@user.id)
+    end
+    redirect_to root_url
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
